71e68692691298b936f6b9512cbb7205242aa601
Anwesh43 <anweshthecool0@gmail.com>
2019-06-21 11:08:59 +0530

    adding HRTVLNode class

--- horizrottovertlineview/src/main/java/com/anwesh/uiprojects/horizrottovertlineview/HorizRotToVertLineView.kt
+++ horizrottovertlineview/src/main/java/com/anwesh/uiprojects/horizrottovertlineview/HorizRotToVertLineView.kt
@@ -123,4 +123,48 @@ class HorizRotToVertLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HRTVLNode(var i : Int, val state : State = State()) {
+
+        private var next : HRTVLNode? = null
+        private var prev : HRTVLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = HRTVLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHRTVLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HRTVLNode {
+            var curr : HRTVLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
