18204a65562349ea7241ea41204373111994153e
Anwesh43 <anweshthecool0@gmail.com>
2019-06-21 10:39:42 +0530

    adding libraries and constants

--- horizrottovertlineview/src/main/java/com/anwesh/uiprojects/horizrottovertlineview/HorizRotToVertLineView.kt
+++ horizrottovertlineview/src/main/java/com/anwesh/uiprojects/horizrottovertlineview/HorizRotToVertLineView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.horizrottovertlineview
 /**
  * Created by anweshmishra on 21/06/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#311B92")
+val backColor : Int = Color.parseColor("#BDBDBD")
