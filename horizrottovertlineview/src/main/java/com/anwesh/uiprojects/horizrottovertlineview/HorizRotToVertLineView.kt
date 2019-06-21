37cc9c9428a729b3750fa58e154bf1299c162bbb
Anwesh43 <anweshthecool0@gmail.com>
2019-06-21 10:59:12 +0530

    adding drawHRTVLNode canvas extension methodd

--- horizrottovertlineview/src/main/java/com/anwesh/uiprojects/horizrottovertlineview/HorizRotToVertLineView.kt
+++ horizrottovertlineview/src/main/java/com/anwesh/uiprojects/horizrottovertlineview/HorizRotToVertLineView.kt
@@ -30,3 +30,32 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawHorizRotToVert(i : Int, size : Float, sc1 : Float, sc2 : Float, paint : Paint) {
+    val sf : Float = 1f - 2 * i
+    val sc1i : Float = sc1.divideScale(i, lines)
+    val sc2i : Float = sc2.divideScale(i, lines)
+    save()
+    translate((size - size * sc2i) * sf, 0f)
+    rotate(90f * sf * sc1i)
+    drawLine(0f, 0f, -size * sf, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawHRTVLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w / 2, gap * (i + 1))
+    for (j in 0..(lines - 1)) {
+        drawHorizRotToVert(j, size, sc1, sc2, paint)
+    }
+    restore()
+}
